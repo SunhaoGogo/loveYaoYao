97cc366dde1d94e6c160b74edcacafdbcc477c53
hsun <313657978@qq.com>
2022-02-14 16:04:17 +0800

    文案修改

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,9 @@
 const CONFIGDATA = {
-  titleHeader: " I Love You",
-  descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
+  titleHeader: "瑶瑶 I Love You",
+  descriptionHeader: "你愿意永远当我女人么 ^^",
   buttonYes: "我愿意",
   buttonNo: "对不起，你不适合我",
-  titleModar: "Cảm ơn em vì đồng ý",
+  titleModar: "瑶瑶",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    "我的心动女孩，愿你永远开心，你是我见过事业心最强的女孩，相信你会在不久的将来达到你满意的高度。我不知道还能陪伴你好久（希望不要再走老路），但我知道我会永远记得你。感觉很多话想对你说，但又无处下手，因为平时说的够多了，哈哈哈，加油喔，节日快乐！！！",
 };
