82c250aea55029ed4429f48bef0d1af4208561b9
hsun <313657978@qq.com>
2022-02-14 15:23:24 +0800

    项目初始化

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: " I Love You",
+  descriptionHeader:
+    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
+  buttonYes: "我愿意",
+  buttonNo: "对不起，你不适合我",
+  titleModar: "Cảm ơn em vì đồng ý",
+  descriptionModar:
+    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+};
